5efcb0fc3769b9e911e84ae9efd8d1c5f8031ceb
LucasNF22 <lucasnfiorentino@gmail.com>
2022-12-07 15:56:09 -0300

    Inicio de pagina web Festival_Musica

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, watch } = require( 'gulp' );
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require( 'gulp-plumber' );
+
+function css( done ) {
+    
+    src('src/scss/**/*.scss')            // Identificar archivo de SASS
+        .pipe( plumber() ) 
+        .pipe( sass() )                 // Compilar
+        .pipe( dest( "build/css" ) )    // Guardar
+
+
+    done();
+};
+
+function dev( done ) {
+    watch('src/scss/**/*.scss', css)
+    done();
+};
+
+exports.css = css;
+exports.dev = dev;
